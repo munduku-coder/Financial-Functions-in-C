5d99d9c7321a46b690ce3e2d518e8f4e09837e84
MUNDUKU INNOCENT <mundukuinnocent2000@gmail.com>
2024-12-28 23:34:23 -0700

    source code for 1,2,3, and 3
    Total Sale Price Calculation, Final Salary After Tax Gross Profit, and ROI

--- Group-one-codes-questions1-4/main.c
+++ Group-one-codes-questions1-4/main.c
@@ -17,3 +17,28 @@
     printf("\nEnter the tax rate: ");
     scanf("%f", &tax_rate);
     salary_after_tax = original_salary - (original_salary * tax_rate);
+//Question number three: Calculate Gross Profit
+    void calculateGrossProfit() {
+    float sales_revenue, cogs, gross_profit;
+    printf("\nEnter the sales revenue (UGX): ");
+    scanf("%f", &sales_revenue);
+    printf("\nEnter the cost of goods sold (COGS) (UGX): ");
+    scanf("%f", &cogs);
+    gross_profit = sales_revenue - cogs;
+    printf("\nThe gross profit is: UGX %.2f\n", gross_profit);}
+    //Question number four: Calculate ROI
+    void calculateROI() {
+    float investment, earnings, roi;
+    printf("\nEnter the total investment (UGX): ");
+    scanf("%f", &investment);
+    printf("\nEnter the total earnings from the investment (UGX): ");
+    scanf("%f", &earnings);
+    roi = ((earnings - investment) / investment) * 100;
+    printf("\nThe ROI is: %.2f%%\n", roi);}
+    int main() {
+    printf("Group 1: Questions 1-4\n");
+    calculateSalePrice();
+    calculateFinalSalary();
+    calculateGrossProfit();
+    calculateROI();
+    return 0;}
